feb1c8fbe697cd435f92246970125a3bb84abe4e
Mutemind78 <113151444+Mutemind78@users.noreply.github.com>
2023-04-09 03:05:45 -0400

    commit 1

---
+++ shared/footer.php
@@ -0,0 +1,4 @@
+<footer>
+	<p>&copy; <?php echo date("Y"); ?> Cricket Management System - BCCI. All rights reserved.</p>
+	<p>Contact: support@cricketmanagementsystem.com</p>
+</footer>
---
+++ shared/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cricket Management System</title>
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="home.php">BCCI</a></li>
+				<li><a href="newplayer.php">New Player</a></li>
+				<li><a href="players.php">Players</a></li>
+				<li><a href="teams.php">Teams</a></li>
+				<li><a href="matchschedule.php">Match Schedule</a></li>
+			</ul>
+			<?php
+				session_start();
+				if(isset($_SESSION['player_id']) && $_SESSION['status'] == "logged_in"){
+					echo '<a href="logout.php">Logout</a>';
+				}else{
+					echo '<a href="login.php">Login</a>';
+				}
+			?>
+		</nav>
+	</header>
+
+  
